ede5b6823fb688d3193843c61b0f03dea7aef810
theesan-amg <theesan270@outlook.com>
2021-08-19 14:06:11 +0200

    Exo 01 App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <>
-    <h1>hello</h1>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: "",
+      password: "",
+      rememberMe: false,
+      emailIsValid: false,
+      passwordIsValid: false,
+      isSubmitted: false
+    }
+
+
+
+  }
+
+  render() {
+    return (
+      <>
+        <h1>hello</h1>
+      </>
+    );
+  }
 }
 
 export default App;
